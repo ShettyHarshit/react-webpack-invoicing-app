82a8e52c9cc070ac9406a730154e846082c637ef
Harshit Shetty <swaharsh@gmail.com>
2025-03-24 01:01:18 +0530

    Update styling and structure for Invoice Management System

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
 import "./App.css";
-import ReactLogo from "../assets/images/react_logo.png";
-import TestVideo from "../assets/videos/test.mp4";
-import { Counter } from "./Counter";
 
 export function App() {
   return (
     <div>
-      <Counter />
-      This is my first App, let see auto reload <img src={ReactLogo} />{" "}
-      <video src={TestVideo} controls />
+      <h1>Invoice Management System</h1>
     </div>
   );
 }
