cdb787db4aecf64f73d9f2869ae65405b9a153db
Harshit Shetty <swaharsh@gmail.com>
2025-03-25 01:09:47 +0530

    feat: add UI helper functions for rendering invoice status tags and formatted dates

---
+++ src/helpers/uiHelpers.js
@@ -0,0 +1,32 @@
+import { Tag } from "antd";
+import React from "react";
+import { InvoiceStatuses } from "../constants/InvoiceStatuses";
+
+export const renderTag = (status) => {
+  let color;
+  switch (status) {
+    case InvoiceStatuses.DRAFT:
+      color = "blue";
+      break;
+    case InvoiceStatuses.SENT:
+      color = "orange";
+      break;
+    case InvoiceStatuses.PAID:
+      color = "green";
+      break;
+    case InvoiceStatuses.OVERDUE:
+      color = "red";
+      break;
+    default:
+      color = "gray";
+  }
+  return <Tag color={color}>{status.toUpperCase()}</Tag>;
+};
+
+export const renderDate = (date) => {
+  return new Date(date).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+};
