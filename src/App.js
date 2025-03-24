ad3ec9d88c3959e6b236646babf250276122e3f8
Harshit Shetty <swaharsh@gmail.com>
2025-03-24 23:23:06 +0530

    refactor: UI Layout + add date formatting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { Button, message, Modal, Select, Space, Table, Tabs, Tag } from "antd";
+import { Button, message, Modal, Select, Space, Table, Tag } from "antd";
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import InvoiceForm from "./components/InvoiceForm";
 import { InvoiceStore } from "./stores/invoiceStore";
 
 export function App() {
@@ -69,159 +68,150 @@ export function App() {
     });
   };
 
-  const items = [
-    {
-      key: "all_invoices",
-      label: "All Invoices",
-      children: (
-        <>
-          <Button
-            onClick={() => {
-              setCurrentTab("create_invoice");
-            }}
-          >
-            Add Invoice
-          </Button>
-          <Table dataSource={invoices} rowKey="id">
-            <Table.Column
-              title="Invoice ID"
-              dataIndex="invoiceNumber"
-              key="id"
-            />
-            <Table.Column title="Client" dataIndex="clientName" key="name" />
-            <Table.Column
-              title="Amount"
-              dataIndex="totalAmount"
-              key="totalAmount"
-              render={(totalAmount) => `₹${totalAmount}`}
-            />
-            <Table.Column
-              title="Status"
-              dataIndex="status"
-              key="status"
-              render={(status) => {
-                let color;
-                switch (status) {
-                  case "draft":
-                    color = "blue";
-                    break;
-                  case "sent":
-                    color = "orange";
-                    break;
-                  case "paid":
-                    color = "green";
-                    break;
-                  case "overdue":
-                    color = "red";
-                    break;
-                  default:
-                    color = "gray";
-                }
-                return <Tag color={color}>{status.toUpperCase()}</Tag>;
-              }}
-            />
-            <Table.Column
-              title="Action"
-              key="action"
-              render={(text, record) => (
-                <Space>
-                  <Button
-                    onClick={() => {
-                      onModalOpen(text, record);
-                    }}
-                  >
-                    View
-                  </Button>
-                  {record.status === "sent" || record.status == "overdue" ? (
-                    <Button
-                      onClick={() => {
-                        message.success(
-                          `Invoice ${record.invoiceNumber} marked as paid`
-                        );
-                        InvoiceStore.updateInvoiceStatus(record.id, "paid");
-                        const fetchedInvoices = InvoiceStore.getInvoices();
-                        setInvoices(fetchedInvoices);
-                      }}
-                    >
-                      Mark as Paid
-                    </Button>
-                  ) : null}
-                  {record.status === "sent" ? (
-                    <Button
-                      onClick={() => {
-                        message.warning(
-                          `Invoice ${record.invoiceNumber} marked as overdue`
-                        );
-                        InvoiceStore.updateInvoiceStatus(record.id, "overdue");
-                        const fetchedInvoices = InvoiceStore.getInvoices();
-                        setInvoices(fetchedInvoices);
-                      }}
-                    >
-                      Mark as Overdue
-                    </Button>
-                  ) : null}
-                  {record.status === "draft" && (
-                    <Button
-                      onClick={() => {
-                        message.success(
-                          `Invoice sent successfully for ${record.invoiceNumber} to ${record.clientEmail}`
-                        );
-                        InvoiceStore.updateInvoiceStatus(record.id, "sent");
-                        const fetchedInvoices = InvoiceStore.getInvoices();
-                        setInvoices(fetchedInvoices);
-                      }}
-                    >
-                      Send Invoice
-                    </Button>
-                  )}
-                  <Button
-                    danger
-                    onClick={() => {
-                      Modal.confirm({
-                        title: "Are you sure you want to delete this invoice?",
-                        onOk() {
-                          InvoiceStore.deleteInvoice(record.id);
-                          const fetchedInvoices = InvoiceStore.getInvoices();
-                          setInvoices(fetchedInvoices);
-                          message.success(
-                            `Invoice ${record.invoiceNumber} deleted successfully`
-                          );
-                        },
-                      });
-                    }}
-                  >
-                    Delete
-                  </Button>
-                </Space>
-              )}
-            />
-          </Table>
-        </>
-      ),
-    },
-    {
-      key: "create_invoice",
-      label: "Create Invoice",
-      children: (
-        <InvoiceForm
-          handleAfterSubmit={() => {
-            setCurrentTab("all_invoices");
-          }}
-        />
-      ),
-    },
-  ];
-
   return (
     <div className="container mx-auto py-8">
       <h1>Invoice Management System</h1>
-      <Tabs
-        onChange={(key) => {
-          setCurrentTab(key);
+      <Button
+        onClick={() => {
+          setCurrentTab("create_invoice");
         }}
-        activeKey={currentTab}
-        items={items}
-      />
+      >
+        Add Invoice
+      </Button>
+      <Table dataSource={invoices} rowKey="id">
+        <Table.Column title="Invoice ID" dataIndex="invoiceNumber" key="id" />
+        <Table.Column title="Client" dataIndex="clientName" key="name" />
+        <Table.Column
+          title="Invoice Date"
+          dataIndex="invoiceDate"
+          key="invoiceDate"
+          render={(date) =>
+            new Date(date).toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            })
+          }
+        />
+        <Table.Column
+          title="Due Date"
+          dataIndex="dueDate"
+          key="dueDate"
+          render={(date) =>
+            new Date(date).toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            })
+          }
+        />
+        <Table.Column
+          title="Amount"
+          dataIndex="totalAmount"
+          key="totalAmount"
+          render={(totalAmount) => `₹${totalAmount}`}
+        />
+        <Table.Column
+          title="Status"
+          dataIndex="status"
+          key="status"
+          render={(status) => {
+            let color;
+            switch (status) {
+              case "draft":
+                color = "blue";
+                break;
+              case "sent":
+                color = "orange";
+                break;
+              case "paid":
+                color = "green";
+                break;
+              case "overdue":
+                color = "red";
+                break;
+              default:
+                color = "gray";
+            }
+            return <Tag color={color}>{status.toUpperCase()}</Tag>;
+          }}
+        />
+        <Table.Column
+          title="Action"
+          key="action"
+          render={(text, record) => (
+            <Space>
+              <Button
+                onClick={() => {
+                  onModalOpen(text, record);
+                }}
+              >
+                View
+              </Button>
+              {record.status === "sent" || record.status == "overdue" ? (
+                <Button
+                  onClick={() => {
+                    message.success(
+                      `Invoice ${record.invoiceNumber} marked as paid`
+                    );
+                    InvoiceStore.updateInvoiceStatus(record.id, "paid");
+                    const fetchedInvoices = InvoiceStore.getInvoices();
+                    setInvoices(fetchedInvoices);
+                  }}
+                >
+                  Mark as Paid
+                </Button>
+              ) : null}
+              {record.status === "sent" ? (
+                <Button
+                  onClick={() => {
+                    message.warning(
+                      `Invoice ${record.invoiceNumber} marked as overdue`
+                    );
+                    InvoiceStore.updateInvoiceStatus(record.id, "overdue");
+                    const fetchedInvoices = InvoiceStore.getInvoices();
+                    setInvoices(fetchedInvoices);
+                  }}
+                >
+                  Mark as Overdue
+                </Button>
+              ) : null}
+              {record.status === "draft" && (
+                <Button
+                  onClick={() => {
+                    message.success(
+                      `Invoice sent successfully for ${record.invoiceNumber} to ${record.clientEmail}`
+                    );
+                    InvoiceStore.updateInvoiceStatus(record.id, "sent");
+                    const fetchedInvoices = InvoiceStore.getInvoices();
+                    setInvoices(fetchedInvoices);
+                  }}
+                >
+                  Send Invoice
+                </Button>
+              )}
+              <Button
+                danger
+                onClick={() => {
+                  Modal.confirm({
+                    title: "Are you sure you want to delete this invoice?",
+                    onOk() {
+                      InvoiceStore.deleteInvoice(record.id);
+                      const fetchedInvoices = InvoiceStore.getInvoices();
+                      setInvoices(fetchedInvoices);
+                      message.success(
+                        `Invoice ${record.invoiceNumber} deleted successfully`
+                      );
+                    },
+                  });
+                }}
+              >
+                Delete
+              </Button>
+            </Space>
+          )}
+        />
+      </Table>
     </div>
   );
 }
